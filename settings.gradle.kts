65f01710fc139e985baba43feb73d77196c70989
Roman Zavarnitsyn <rom4ek93@gmail.com>
2021-08-10 17:18:45 +0200

    Moar dependency updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,16 +7,12 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         google()
-        jcenter()
-        maven {
-            url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
-        }
     }
     // We need this to be able to resolve plugin "com.android.library"
     resolutionStrategy {
         eachPlugin {
-            if (requested.id.namespace == "com.android" || requested.id.name == "kotlin-android-extensions") {
-                useModule("com.android.tools.build:gradle:3.5.2")
+            if (requested.id.namespace == "com.android") {
+                useModule("com.android.tools.build:gradle:4.2.0")
             }
         }
     }
