305b5ff12275287900d42c9e6168c740d53912d7
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-07-19 08:18:29 -0700

    Add distribution plugin

---
+++ buildSrc/src/main/java/Publication.kt
@@ -0,0 +1,66 @@
+import org.gradle.api.Project
+import org.gradle.api.distribution.DistributionContainer
+import java.io.File
+
+private object Consts {
+    val taskRegex = Regex("(.*)DistZip")
+}
+
+// configure distZip tasks for multiplatform
+fun DistributionContainer.configureForMultiplatform(project: Project) {
+    val sep = File.separator
+
+    this.maybeCreate("android").contents {
+        from("build${sep}publications${sep}androidRelease")
+        from("build${sep}outputs${sep}aar${sep}sentry-kotlin-multiplatform-release.aar") {
+            rename {
+                it.replace("-release", "-android-release")
+            }
+        }
+        from("build${sep}libs") {
+            include("*android*")
+            exclude("*debug*")
+        }
+    }
+    this.getByName("main").contents {
+        from("build${sep}publications${sep}kotlinMultiplatform")
+        from("build${sep}kotlinToolingMetadata")
+        from("build${sep}libs") {
+            include("*kotlin*")
+            include("*metadata*")
+        }
+    }
+    this.maybeCreate("jvm").contents {
+        from("build${sep}publications${sep}jvm")
+        from("build${sep}libs${sep}") {
+            include("*jvm*")
+        }
+    }
+    this.maybeCreate("iosArm64").contents {
+        from("build${sep}publications${sep}iosArm64")
+        from("build${sep}libs${sep}iosArm64")
+        from("build${sep}libs${sep}") {
+            include("sentry-kotlin-multiplatform-iosarm64*")
+        }
+    }
+    this.maybeCreate("iosX64").contents {
+        from("build${sep}publications${sep}iosX64")
+        from("build${sep}libs${sep}iosX64")
+        from("build${sep}libs${sep}") {
+            include("sentry-kotlin-multiplatform-iosx64*")
+        }
+    }
+    this.maybeCreate("iosSimulatorArm64").contents {
+        from("build${sep}publications${sep}iosSimulatorArm64")
+        from("build${sep}libs${sep}iosSimulatorArm64")
+        from("build${sep}libs${sep}") {
+            include("sentry-kotlin-multiplatform-iossimulatorarm64*")
+        }
+    }
+
+    // make other distZip tasks run together with the main distZip
+    val platformDists = project.tasks.filter { task ->
+        task.name.matches(Consts.taskRegex)
+    }.toTypedArray()
+    project.tasks.getByName("distZip").finalizedBy(*platformDists)
+}
