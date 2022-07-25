ce27388269f9262710b609edaa2eedad18565fc3
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-07-25 08:02:47 -0700

    Fix build

--- sentry-kotlin-multiplatform-nsexceptions/build.gradle.kts
+++ sentry-kotlin-multiplatform-nsexceptions/build.gradle.kts
@@ -1,36 +1,30 @@
 plugins {
     kotlin("multiplatform")
+    `maven-publish`
 }
 
 kotlin {
     explicitApi()
 
-    val macosX64 = macosX64()
-    val macosArm64 = macosArm64()
     val iosArm64 = iosArm64()
     val iosX64 = iosX64()
     val iosSimulatorArm64 = iosSimulatorArm64()
-    val watchosArm32 = watchosArm32()
-    val watchosArm64 = watchosArm64()
-    val watchosX64 = watchosX64()
-    val watchosSimulatorArm64 = watchosSimulatorArm64()
-    val tvosArm64 = tvosArm64()
-    val tvosX64 = tvosX64()
-    val tvosSimulatorArm64 = tvosSimulatorArm64()
 
     sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
+        ios()
+        iosSimulatorArm64()
+
+        val appleMain by creating
+        val iosMain by getting { dependsOn(appleMain) }
+        val iosSimulatorArm64Main by getting { dependsOn(appleMain) }
+        val appleTest by creating {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
     }
     listOf(
-        macosX64, macosArm64,
         iosArm64, iosX64, iosSimulatorArm64,
-        watchosArm32, watchosArm64, watchosX64, watchosSimulatorArm64,
-        tvosArm64, tvosX64, tvosSimulatorArm64
     ).forEach {
         it.compilations.getByName("main") {
             cinterops.create("NSExceptions.Sentry") {
