f2e2c137cff51b9578586efd91c0cdc5d08665b0
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-07-28 00:12:45 -0700

    Ref code

--- sentry-samples/kmp-app/androidApp/src/main/java/sample/kpm_app/android/MainActivity.kt
+++ sentry-samples/kmp-app/androidApp/src/main/java/sample/kpm_app/android/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
 class SentryApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        Sentry.init(context = this) {
+        Sentry.init(this) {
             it.dsn = "https://83f281ded2844eda83a8a413b080dbb9@o447951.ingest.sentry.io/5903800"
             it.attachStackTrace = true
             it.attachThreads = true
