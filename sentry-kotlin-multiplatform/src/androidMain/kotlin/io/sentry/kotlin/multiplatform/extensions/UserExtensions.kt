2dd90c79f88ae1b8f670babbdce3cb2d1f90a250
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-08-04 04:39:08 -0700

    Use toMutableMap

--- sentry-kotlin-multiplatform/src/androidMain/kotlin/io/sentry/kotlin/multiplatform/extensions/UserExtensions.kt
+++ sentry-kotlin-multiplatform/src/androidMain/kotlin/io/sentry/kotlin/multiplatform/extensions/UserExtensions.kt
@@ -3,7 +3,6 @@ package io.sentry.kotlin.multiplatform.extensions
 import io.sentry.kotlin.multiplatform.AndroidUser
 import io.sentry.kotlin.multiplatform.protocol.ISentryUser
 import io.sentry.kotlin.multiplatform.protocol.User
-import io.sentry.util.CollectionUtils
 
 fun ISentryUser.toAndroidUser(): AndroidUser {
     val androidUser = AndroidUser()
@@ -11,8 +10,8 @@ fun ISentryUser.toAndroidUser(): AndroidUser {
     androidUser.username = this.username
     androidUser.email = this.email
     androidUser.ipAddress = this.ipAddress
-    androidUser.others = CollectionUtils.newConcurrentHashMap(this.other)
-    androidUser.unknown = CollectionUtils.newConcurrentHashMap(this.unknown)
+    androidUser.others = this.other.toMutableMap()
+    androidUser.unknown = this.unknown.toMutableMap()
     return androidUser
 }
 
@@ -22,10 +21,10 @@ fun AndroidUser.toKmpUser(): User {
     kmpUser.username = this.username.toString()
     kmpUser.email = this.email.toString()
     kmpUser.ipAddress = this.ipAddress.toString()
-    CollectionUtils.newConcurrentHashMap(this.others)?.let {
+    this.others?.toMutableMap()?.let {
         kmpUser.other = it
     }
-    CollectionUtils.newConcurrentHashMap(this.unknown)?.let {
+    this.unknown?.toMutableMap()?.let {
         kmpUser.unknown = it
     }
     return kmpUser
