c50e2437b1f77d79fe1e6abf3eca804a3061238c
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-08-04 04:35:51 -0700

    Fix import

--- sentry-kotlin-multiplatform/src/appleMain/kotlin/io/sentry/kotlin/multiplatform/extensions/SentryOptionsExtension.kt
+++ sentry-kotlin-multiplatform/src/appleMain/kotlin/io/sentry/kotlin/multiplatform/extensions/SentryOptionsExtension.kt
@@ -2,10 +2,11 @@ package io.sentry.kotlin.multiplatform.extensions
 
 import cocoapods.Sentry.SentryEvent
 import io.sentry.kotlin.multiplatform.CocoaSentryOptions
+import io.sentry.kotlin.multiplatform.SentryOptions
 import io.sentry.kotlin.multiplatform.nsexception.dropKotlinCrashEvent
 import NSException.Sentry.SentryEvent as NSExceptionSentryEvent
 
-fun io.sentry.kotlin.multiplatform.SentryOptions.toCocoaSentryOptions(): CocoaSentryOptions {
+fun SentryOptions.toCocoaSentryOptions(): CocoaSentryOptions {
     val sentryAppleOptions = CocoaSentryOptions()
     sentryAppleOptions.dsn = this.dsn
     sentryAppleOptions.attachStacktrace = this.attachStackTrace
