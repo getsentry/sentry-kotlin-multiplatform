18d689bb871b544800c4ab30bb4af5a76f0616c0
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-08-06 04:49:55 -0700

    Remove compose app (doesnt work in multi projects with kmp

--- sentry-samples/compose-multiplatform-app/common/src/commonMain/kotlin/com/example/common/appInit.kt
+++
@@ -1,4 +0,0 @@
-package com.example.common
-
-expect fun sentryInit()
-
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,9 +14,3 @@ include(":sentry-kotlin-multiplatform")
 // KMP Sample App with Android, iOS
 include("sentry-samples:kmp-app:shared")
 include("sentry-samples:kmp-app:androidApp")
-include("sentry-samples:compose-multiplatform-app")
-
-// Compose Multiplatform with Android, Desktop JVM, iOS SwiftUI
-include("sentry-samples:compose-multiplatform-app:common")
-include("sentry-samples:compose-multiplatform-app:android")
-include("sentry-samples:compose-multiplatform-app:desktop")
