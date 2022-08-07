20017e8c6f418f2263e4c40b0371aedf1c3ac725
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-08-07 12:27:12 -0700

    Revert js

--- sentry-kotlin-multiplatform/src/jsMain/kotlin/io/sentry/kotlin/multiplatform/SentryJs.kt
+++ sentry-kotlin-multiplatform/src/jsMain/kotlin/io/sentry/kotlin/multiplatform/SentryJs.kt
@@ -1,62 +1,20 @@
 package io.sentry.kotlin.multiplatform
 
-import io.sentry.kotlin.multiplatform.protocol.SentryId
-
-external interface BrowserOptions
-
-class BrowserOptionsImpl: BrowserOptions
-
-
-@JsModule("@sentry/browser")
-@JsNonModule
-@JsName("Sentry")
-external object SentryJs {
-    interface BrowserOptions
-
-    fun init(options: BrowserOptions = definedExternally)
-
-    fun captureMessage(message: String)
-
-    fun captureException(exception: Any)
-}
-
-fun Sentry.init(configuration: (SentryOptions) -> Unit) {
-    val options = SentryOptions()
-    configuration.invoke(options)
-    val jsOptions: dynamic = BrowserOptionsImpl()
-    jsOptions.dsn = options.dsn
-    SentryJs.init(jsOptions)
-}
-
 internal actual object SentryBridge {
 
-    actual fun captureMessage(message: String): SentryId {
-        SentryJs.captureMessage(message)
-        return SentryId.EMPTY_ID
+    actual fun start(context: Any?, configuration: OptionsConfiguration<SentryKMPOptions>) {
+
     }
 
-    actual fun captureMessage(
-        message: String,
-        scopeCallback: (Scope) -> Unit
-    ): SentryId {
+    actual fun captureMessage(message: String): SentryId {
         return SentryId.EMPTY_ID
     }
 
     actual fun captureException(throwable: Throwable): SentryId {
-        SentryJs.captureException(throwable)
         return SentryId.EMPTY_ID
-    }
-
-    actual fun captureException(
-        throwable: Throwable,
-        scopeCallback: (Scope) -> Unit
-    ): SentryId {
-        return SentryId.EMPTY_ID
-    }
-
-    actual fun configureScope(scopeCallback: (Scope) -> Unit) {
     }
 
     actual fun close() {
+
     }
 }
