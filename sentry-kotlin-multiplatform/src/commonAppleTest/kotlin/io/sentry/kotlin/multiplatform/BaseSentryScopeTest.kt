d0b827fc28c20bb396163dd12f67fc6f90049000
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-08-18 02:55:18 -0700

    Use cocoapods for import

--- sentry-kotlin-multiplatform/src/commonAppleTest/kotlin/io/sentry/kotlin/multiplatform/BaseSentryScopeTest.kt
+++ sentry-kotlin-multiplatform/src/commonAppleTest/kotlin/io/sentry/kotlin/multiplatform/BaseSentryScopeTest.kt
@@ -1,8 +1,10 @@
 package io.sentry.kotlin.multiplatform
 
+import cocoapods.Sentry.SentryScope
+
 actual abstract class BaseSentryScopeTest {
     actual fun initializeScope(): Scope {
-        val cocoaScope = cocoapods.Sentry.SentryScope()
+        val cocoaScope = SentryScope()
         val scopeCocoaImpl = ScopeCocoaImpl(cocoaScope)
         return Scope(scopeCocoaImpl)
     }
