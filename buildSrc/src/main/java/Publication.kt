d718fcf387c820dec962752c58954036a86915bb
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-08-18 03:17:21 -0700

    Change into main directory for apple targets

--- buildSrc/src/main/java/Publication.kt
+++ buildSrc/src/main/java/Publication.kt
@@ -38,84 +38,84 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
     }
     this.maybeCreate("iosArm64").contents {
         from("build${sep}publications${sep}iosArm64")
-        from("build${sep}libs${sep}iosArm64")
+        from("build${sep}libs${sep}iosArm64${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-iosarm64*")
         }
     }
     this.maybeCreate("iosX64").contents {
         from("build${sep}publications${sep}iosX64")
-        from("build${sep}libs${sep}iosX64")
+        from("build${sep}libs${sep}iosX64${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-iosx64*")
         }
     }
     this.maybeCreate("iosSimulatorArm64").contents {
         from("build${sep}publications${sep}iosSimulatorArm64")
-        from("build${sep}libs${sep}iosSimulatorArm64")
+        from("build${sep}libs${sep}iosSimulatorArm64${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-iossimulatorarm64*")
         }
     }
     this.maybeCreate("macosArm64").contents {
         from("build${sep}publications${sep}macosArm64")
-        from("build${sep}libs${sep}macosArm64")
+        from("build${sep}libs${sep}macosArm64${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-macosarm64*")
         }
     }
     this.maybeCreate("macosX64").contents {
         from("build${sep}publications${sep}macosX64")
-        from("build${sep}libs${sep}macosX64")
+        from("build${sep}libs${sep}macosX64${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-macosx64*")
         }
     }
     this.maybeCreate("watchosX64").contents {
         from("build${sep}publications${sep}watchosX64")
-        from("build${sep}libs${sep}watchosX64")
+        from("build${sep}libs${sep}watchosX64${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-watchosx64*")
         }
     }
     this.maybeCreate("watchosArm32").contents {
         from("build${sep}publications${sep}watchosArm32")
-        from("build${sep}libs${sep}watchosArm32")
+        from("build${sep}libs${sep}watchosArm32${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-watchosarm32*")
         }
     }
     this.maybeCreate("watchosArm64").contents {
         from("build${sep}publications${sep}watchosArm64")
-        from("build${sep}libs${sep}watchosArm64")
+        from("build${sep}libs${sep}watchosArm64${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-watchosarm64*")
         }
     }
     this.maybeCreate("watchosSimulatorArm64").contents {
         from("build${sep}publications${sep}watchosSimulatorArm64")
-        from("build${sep}libs${sep}watchosArm64")
+        from("build${sep}libs${sep}watchosArm64${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-watchossimulatorarm64*")
         }
     }
     this.maybeCreate("tvosArm64").contents {
         from("build${sep}publications${sep}tvosArm64")
-        from("build${sep}libs${sep}tvosArm64")
+        from("build${sep}libs${sep}tvosArm64${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-tvosarm64*")
         }
     }
     this.maybeCreate("tvosX64").contents {
         from("build${sep}publications${sep}tvosX64")
-        from("build${sep}libs${sep}tvosX64")
+        from("build${sep}libs${sep}tvosX64${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-tvosx64*")
         }
     }
     this.maybeCreate("tvosSimulatorArm64").contents {
         from("build${sep}publications${sep}tvosSimulatorArm64")
-        from("build${sep}libs${sep}tvosArm64")
+        from("build${sep}libs${sep}tvosArm64${sep}main")
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-tvossimulatorarm64*")
         }
