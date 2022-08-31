1529cff1a43010f8553b0788181b514780780b02
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-08-31 08:21:08 -0700

    Rename module.json in distribution

--- buildSrc/src/main/java/Publication.kt
+++ buildSrc/src/main/java/Publication.kt
@@ -14,7 +14,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
     val version = project.properties["versionName"].toString()
 
     this.maybeCreate("android").contents {
-        from("build${sep}publications${sep}androidRelease")
+        from("build${sep}publications${sep}androidRelease") {
+            renameModule("android", version)
+        }
         from("build${sep}outputs${sep}aar${sep}sentry-kotlin-multiplatform-release.aar") {
             rename {
                 it.replace("-release", "-android-release")
@@ -26,7 +28,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         }
     }
     this.getByName("main").contents {
-        from("build${sep}publications${sep}kotlinMultiplatform")
+        from("build${sep}publications${sep}kotlinMultiplatform") {
+            renameModule(version = version)
+        }
         from("build${sep}kotlinToolingMetadata")
         from("build${sep}libs") {
             include("sentry-kotlin-multiplatform-?.?.*")
@@ -38,14 +42,18 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         }
     }
     this.maybeCreate("jvm").contents {
-        from("build${sep}publications${sep}jvm")
+        from("build${sep}publications${sep}jvm") {
+            renameModule("jvm", version)
+        }
         from("build${sep}libs$sep") {
             include("*jvm*")
             withJavadoc("jvm")
         }
     }
     this.maybeCreate("iosArm64").contents {
-        from("build${sep}publications${sep}iosArm64")
+        from("build${sep}publications${sep}iosArm64") {
+            renameModule("iosarm64", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-iosarm64*")
             withJavadoc("iosarm64")
@@ -53,7 +61,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         fromKlib("iosArm64", version)
     }
     this.maybeCreate("iosX64").contents {
-        from("build${sep}publications${sep}iosX64")
+        from("build${sep}publications${sep}iosX64") {
+            renameModule("iosx64", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-iosx64*")
             withJavadoc("iosx64")
@@ -61,7 +71,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         fromKlib("iosX64", version)
     }
     this.maybeCreate("iosSimulatorArm64").contents {
-        from("build${sep}publications${sep}iosSimulatorArm64")
+        from("build${sep}publications${sep}iosSimulatorArm64") {
+            renameModule("iossimulatorarm64", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-iossimulatorarm64*")
             withJavadoc("iossimulatorarm64")
@@ -69,7 +81,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         fromKlib("iosSimulatorArm64", version)
     }
     this.maybeCreate("macosArm64").contents {
-        from("build${sep}publications${sep}macosArm64")
+        from("build${sep}publications${sep}macosArm64") {
+            renameModule("macosarm64", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-macosarm64*")
             withJavadoc("macosarm64")
@@ -77,7 +91,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         fromKlib("macosArm64", version)
     }
     this.maybeCreate("macosX64").contents {
-        from("build${sep}publications${sep}macosX64")
+        from("build${sep}publications${sep}macosX64") {
+            renameModule("macosx64", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-macosx64*")
             withJavadoc("macosx64")
@@ -85,7 +101,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         fromKlib("macosX64", version)
     }
     this.maybeCreate("watchosX64").contents {
-        from("build${sep}publications${sep}watchosX64")
+        from("build${sep}publications${sep}watchosX64") {
+            renameModule("watchosx64", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-watchosx64*")
             withJavadoc("watchosx64")
@@ -93,7 +111,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         fromKlib("watchosX64", version)
     }
     this.maybeCreate("watchosArm32").contents {
-        from("build${sep}publications${sep}watchosArm32")
+        from("build${sep}publications${sep}watchosArm32") {
+            renameModule("watchosarm32", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-watchosarm32*")
             withJavadoc("watchosarm32")
@@ -101,7 +121,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         fromKlib("watchosArm32", version)
     }
     this.maybeCreate("watchosArm64").contents {
-        from("build${sep}publications${sep}watchosArm64")
+        from("build${sep}publications${sep}watchosArm64") {
+            renameModule("watchosarm64", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-watchosarm64*")
             withJavadoc("watchosarm64")
@@ -109,7 +131,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         fromKlib("watchosArm64", version)
     }
     this.maybeCreate("watchosSimulatorArm64").contents {
-        from("build${sep}publications${sep}watchosSimulatorArm64")
+        from("build${sep}publications${sep}watchosSimulatorArm64") {
+            renameModule("watchossimulatorarm64", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-watchossimulatorarm64*")
             withJavadoc("watchossimulatorarm64")
@@ -117,7 +141,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         fromKlib("watchosSimulatorArm64", version)
     }
     this.maybeCreate("tvosArm64").contents {
-        from("build${sep}publications${sep}tvosArm64")
+        from("build${sep}publications${sep}tvosArm64") {
+            renameModule("tvosarm64", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-tvosarm64*")
             withJavadoc("tvosarm64")
@@ -125,7 +151,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         fromKlib("tvosArm64", version)
     }
     this.maybeCreate("tvosX64").contents {
-        from("build${sep}publications${sep}tvosX64")
+        from("build${sep}publications${sep}tvosX64") {
+            renameModule("tvosx64", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-tvosx64*")
             withJavadoc("tvosx64")
@@ -133,7 +161,9 @@ fun DistributionContainer.configureForMultiplatform(project: Project) {
         fromKlib("tvosX64", version)
     }
     this.maybeCreate("tvosSimulatorArm64").contents {
-        from("build${sep}publications${sep}tvosSimulatorArm64")
+        from("build${sep}publications${sep}tvosSimulatorArm64") {
+            renameModule("tvossimulatorarm64", version)
+        }
         from("build${sep}libs$sep") {
             include("sentry-kotlin-multiplatform-tvossimulatorarm64*")
             withJavadoc("tvossimulatorarm64")
@@ -159,6 +189,16 @@ private fun CopySpec.fromKlib(target: String, version: String) {
         rename {
             "sentry-kotlin-multiplatform-${target.toLowerCase()}-$version.klib"
         }
+    }
+}
+
+private fun CopySpec.renameModule(renameTo: String = "", version: String) {
+    var target = ""
+    if (!renameTo.isEmpty()) {
+        target = "-$renameTo"
+    }
+    rename {
+        it.replace("module.json", "sentry-kotlin-multiplatform${target}-${version}.module")
     }
 }
 
