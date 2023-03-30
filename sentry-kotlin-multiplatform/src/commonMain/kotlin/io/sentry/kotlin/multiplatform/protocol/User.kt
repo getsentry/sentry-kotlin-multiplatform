02901f35119e6bf0a64b9965d1931dd51e3b0a48
buenaflor <giancarlo.buenaflor@sentry.io>
2023-03-30 10:26:34 +0200

    update visibility modifiers and naming

--- sentry-kotlin-multiplatform/src/commonMain/kotlin/io/sentry/kotlin/multiplatform/protocol/User.kt
+++ sentry-kotlin-multiplatform/src/commonMain/kotlin/io/sentry/kotlin/multiplatform/protocol/User.kt
@@ -2,25 +2,25 @@ package io.sentry.kotlin.multiplatform.protocol
 
 public data class User(
     /** The user's email */
-    public var email: String = "",
+    var email: String = "",
 
     /** The user's id */
-    public var id: String = "",
+    var id: String = "",
 
     /** The user's username */
-    public var username: String = "",
+    var username: String = "",
 
     /** The user's ip address*/
-    public var ipAddress: String? = null,
+    var ipAddress: String? = null,
 
     /**
      * Additional arbitrary fields, as stored in the database (and sometimes as sent by clients). All
      * data from `self.other` should end up here after store normalization.
      */
-    public var other: MutableMap<String, String>? = null,
+    var other: MutableMap<String, String>? = null,
 
     /** Unknown fields, only internal usage. */
-    public var unknown: MutableMap<String, Any>? = null
+    var unknown: MutableMap<String, Any>? = null
 ) {
 
     public constructor(user: User) : this(
