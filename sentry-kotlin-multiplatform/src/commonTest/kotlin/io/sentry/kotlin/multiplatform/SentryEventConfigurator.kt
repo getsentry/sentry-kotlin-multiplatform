f1f6609e2970de6749d58e2d3fda6726b65cc637
buenaflor <giancarlo.buenaflor@sentry.io>
2023-04-24 12:15:12 +0200

    update docs

--- sentry-kotlin-multiplatform/src/commonTest/kotlin/io/sentry/kotlin/multiplatform/SentryEventConfigurator.kt
+++ sentry-kotlin-multiplatform/src/commonTest/kotlin/io/sentry/kotlin/multiplatform/SentryEventConfigurator.kt
@@ -1,5 +1,9 @@
 package io.sentry.kotlin.multiplatform
 
+/**
+ * This class deals with configuring and modifying a SentryEvent.
+ * It is used to test any code that can alter a SentryEvent.
+ */
 expect class SentryEventConfigurator() {
     val originalEvent: SentryEvent
     fun applyOptions(optionsConfiguration: OptionsConfiguration): SentryEvent?
