e173669ff0c3c2f04968f523eec9ff9f3bf19a5d
buenaflor <giancarlo.buenaflor@sentry.io>
2023-04-24 18:17:21 +0200

    omit roboelectric config

--- sentry-kotlin-multiplatform/src/androidUnitTest/kotlin/io/sentry/kotlin/multiplatform/BaseSentryTest.kt
+++ sentry-kotlin-multiplatform/src/androidUnitTest/kotlin/io/sentry/kotlin/multiplatform/BaseSentryTest.kt
@@ -3,10 +3,8 @@ package io.sentry.kotlin.multiplatform
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
-@Config(sdk = [28])
 actual abstract class BaseSentryTest {
     actual val platform: String = "Android"
     actual val authToken: String? = System.getenv("SENTRY_AUTH_TOKEN")
