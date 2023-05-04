539d6bf7baff6f796390a6346cd93222a3d9383a
buenaflor <giancarlo.buenaflor@sentry.io>
2023-05-04 17:49:07 +0200

    change doc of SpanContexT

--- sentry-kotlin-multiplatform/src/commonMain/kotlin/io/sentry/kotlin/multiplatform/SpanContext.kt
+++ sentry-kotlin-multiplatform/src/commonMain/kotlin/io/sentry/kotlin/multiplatform/SpanContext.kt
@@ -3,22 +3,24 @@ package io.sentry.kotlin.multiplatform
 import io.sentry.kotlin.multiplatform.protocol.SentryId
 import io.sentry.kotlin.multiplatform.protocol.SpanId
 
-/**
- * An interface representing the context of a span.
- *
- * @property operation The name of the operation associated with the span.
- * @property traceId Determines which trace the span belongs to.
- * @property spanId The unique identifier of the span.
- * @property parentSpanId The unique identifier of the span's parent, if any.
- * @property description A longer description of the span's operation, which uniquely identifies the span but is
- * consistent across instances of the span.
- * @property sampled Indicates if the span is sampled.
- */
+/** An interface representing the context of a span. */
 public interface SpanContext {
+    /** The name of the operation associated with the span.*/
     public val operation: String
+
+    /** Determines which trace the span belongs to.*/
     public val traceId: SentryId
+
+    /** The unique identifier of the span.*/
     public val spanId: SpanId
+
+    /** The unique identifier of the span's parent, if any.*/
     public val parentSpanId: SpanId?
+
+    /** A longer description of the span's operation, which uniquely identifies the span but is
+     * consistent across instances of the span.*/
     public val description: String?
+
+    /** Indicates if the span is sampled.*/
     public val sampled: Boolean
 }
