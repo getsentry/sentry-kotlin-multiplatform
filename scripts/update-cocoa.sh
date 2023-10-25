af2b68bcddc215996a029e81b216ef60f9d57bf7
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2023-10-25 15:49:03 +0200

    Try updating both at the same time

--- scripts/update-cocoa.sh
+++ scripts/update-cocoa.sh
@@ -2,51 +2,43 @@
 
 cd $(dirname "$0")/../
 
-# Change version in buildSrc
-file='buildSrc/src/main/java/Config.kt'
-content=$(cat $file)
-regex='(sentryCocoaVersion *= *)"([0-9\.]+)"'
-if ! [[ $content =~ $regex ]]; then
-    echo "Failed to find the Cocoa version in $file"
+config_file='buildSrc/src/main/java/Config.kt'
+podspec_file='sentry-kotlin-multiplatform/sentry_kotlin_multiplatform.podspec'
+
+config_content=$(cat $config_file)
+podspec_content=$(cat $podspec_file)
+
+config_regex='(sentryCocoaVersion *= *)"([0-9\.]+)"'
+podspec_regex="('Sentry', *)'([0-9\.]+)'"
+
+if ! [[ $config_content =~ $config_regex ]]; then
+    echo "Failed to find the Cocoa version in $config_file"
+    exit 1
+fi
+
+config_bash_rematch=$BASH_REMATCH
+
+if ! [[ $podspec_content =~ $podspec_regex ]]; then
+    echo "Failed to find the Cocoa version in $podspec_file"
     exit 1
 fi
 
 case $1 in
 get-version)
-    echo ${BASH_REMATCH[2]}
+    echo ${config_bash_rematch[2]}
+    echo ${podspec_bash_rematch[2]}
     ;;
 get-repo)
     echo "https://github.com/getsentry/sentry-cocoa.git"
     ;;
 set-version)
-    newValue="${BASH_REMATCH[1]}\"$2"\"
-    echo "${content/${BASH_REMATCH[0]}/$newValue}" >$file
-    ;;
-*)
-    echo "Unknown argument $1"
-    exit 1
-    ;;
-esac
+    # Update the version in the config file
+    newValue="${config_bash_rematch[1]}\"$2"\"
+    echo "${config_content/${config_bash_rematch[0]}/$newValue}" >$config_file
 
-# Change podspec version
-file='sentry-kotlin-multiplatform/sentry_kotlin_multiplatform.podspec'
-content=$(cat $file)
-regex="('Sentry', *)'([0-9\.]+)'"
-if ! [[ $content =~ $regex ]]; then
-    echo "Failed to find the Cocoa version in $file"
-    exit 1
-fi
-
-case $1 in
-get-version)
-    echo ${BASH_REMATCH[2]}
-    ;;
-get-repo)
-    echo "https://github.com/getsentry/sentry-cocoa.git"
-    ;;
-set-version)
-    newValue="${BASH_REMATCH[1]}\"$2"\"
-    echo "${content/${BASH_REMATCH[0]}/$newValue}" >$file
+    # Update the version in the podspec file
+    newValue="${podspec_bash_rematch[1]}'$2'"
+    echo "${podspec_content/${podspec_bash_rematch[0]}/$newValue}" >$podspec_file
     ;;
 *)
     echo "Unknown argument $1"
