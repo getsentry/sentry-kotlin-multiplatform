c4e844122ea2b8777f27d83fd1a172517d9b778b
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2023-10-27 15:09:44 +0200

    Update cocoa.sh

--- scripts/update-cocoa.sh
+++ scripts/update-cocoa.sh
@@ -16,30 +16,34 @@ if ! [[ $config_content =~ $config_regex ]]; then
     exit 1
 fi
 
-config_bash_rematch=$BASH_REMATCH
+config_whole_match=${BASH_REMATCH[0]}
+config_var_name=${BASH_REMATCH[1]}
+config_version=${BASH_REMATCH[2]}
 
 if ! [[ $podspec_content =~ $podspec_regex ]]; then
     echo "Failed to find the Cocoa version in $podspec_file"
     exit 1
 fi
 
-podspec_bash_rematch=$BASH_REMATCH
+podspec_whole_match=${BASH_REMATCH[0]}
+podspec_var_name=${BASH_REMATCH[1]}
 
 case $1 in
 get-version)
-    echo ${BASH_REMATCH[2]}
+    # We only require to return the version number of one of the files
+    echo ${config_version}
     ;;
 get-repo)
     echo "https://github.com/getsentry/sentry-cocoa.git"
     ;;
 set-version)
     # Update the version in the config file
-    newValue="${config_bash_rematch[1]}\"$2"\"
-    echo "${config_content/${config_bash_rematch[0]}/$newValue}" >$config_file
+    newValue="${config_var_name}\"$2"\"
+    echo "${config_content/${config_whole_match}/$newValue}" >$config_file
 
     # Update the version in the podspec file
-    newValue="${podspec_bash_rematch[1]}'$2'"
-    echo "${podspec_content/${podspec_bash_rematch[0]}/$newValue}" >$podspec_file
+    newValue="${podspec_var_name}'$2'"
+    echo "${podspec_content/${podspec_whole_match}/$newValue}" >$podspec_file
     ;;
 *)
     echo "Unknown argument $1"
