add53d3fbd3daf30080067671ee227152316567c
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2023-12-04 10:54:25 +0100

    ci: improve speed and refactor (#147)

---
+++ scripts/build-distribution.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Please provide a gradle project name."
+  exit 1
+fi
+
+PROJECT_NAME="$1"
+./gradlew "distzip" -p "${PROJECT_NAME}"
---
+++ scripts/build-jvm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Please provide a gradle project name."
+  exit 1
+fi
+
+PROJECT_NAME="$1"
+./gr
+./gradlew "testDebugUnitTest" \
+          "testReleaseUnitTest" \
+          "publishAndroidReleasePublicationToMavenLocal" \
+          "publishJvmPublicationToMavenLocal" \
+          "publishKotlinMultiplatformPublicationToMavenLocal" \
+          -p "${PROJECT_NAME}"
