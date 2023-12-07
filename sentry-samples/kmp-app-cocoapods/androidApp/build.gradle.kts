18c936bb2641b9a64e01571ed9b124e2547e9229
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2023-12-07 13:31:27 +0100

    Update samples

--- sentry-samples/kmp-app-cocoapods/androidApp/build.gradle.kts
+++ sentry-samples/kmp-app-cocoapods/androidApp/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-    implementation(rootProject.project(":sentry-samples:kmp-app-spm:shared"))
+    implementation(rootProject.project(":sentry-samples:kmp-app-cocoapods:shared"))
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
