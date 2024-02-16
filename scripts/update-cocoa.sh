6b6df5d232456ac7de8b90f35f6b9f0ae0c2f9ac
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-02-16 02:59:16 +0100

    draft

--- scripts/update-cocoa.sh
+++ scripts/update-cocoa.sh
@@ -4,9 +4,13 @@ cd $(dirname "$0")/../
 
 config_file='buildSrc/src/main/java/Config.kt'
 podspec_file='sentry-kotlin-multiplatform/sentry_kotlin_multiplatform.podspec'
+podspec2_file='sentry-samples/kmp-app-cocoapods/shared/shared.podspec'
+project_file='sentry-samples/kmp-app-spm/iosApp.xcodeproj/project.pbxproj'
 
 config_content=$(cat $config_file)
 podspec_content=$(cat $podspec_file)
+podspec2_content=$(cat $podspec2_file)
+project_content=$(cat $project_file)
 
 config_regex='(sentryCocoaVersion *= *)"([0-9\.]+)"'
 podspec_regex="('Sentry', *)'([0-9\.]+)'"
@@ -20,6 +24,8 @@ config_whole_match=${BASH_REMATCH[0]}
 config_var_name=${BASH_REMATCH[1]}
 config_version=${BASH_REMATCH[2]}
 
+project_regex="(version = )(8.17.1)"
+
 if ! [[ $podspec_content =~ $podspec_regex ]]; then
     echo "Failed to find the Cocoa version in $podspec_file"
     exit 1
@@ -27,6 +33,14 @@ fi
 
 podspec_whole_match=${BASH_REMATCH[0]}
 podspec_var_name=${BASH_REMATCH[1]}
+
+if ! [[ $project_content =~ $project_regex ]]; then
+    echo "Failed to find the Cocoa version in $project_file"
+    exit 1
+fi
+
+project_whole_match=${BASH_REMATCH[0]}
+project_var_name=${BASH_REMATCH[1]}
 
 case $1 in
 get-version)
@@ -44,6 +58,14 @@ set-version)
     # Update the version in the podspec file
     newValue="${podspec_var_name}'$2'"
     echo "${podspec_content/${podspec_whole_match}/$newValue}" >$podspec_file
+
+    newValue="${podspec_var_name}'$2'"
+    echo $newValue
+    echo "${project_content/${podspec_whole_match}/$newValue}" >$podspec2_file
+
+    newValue="${project_var_name}$2"
+    echo $newValue
+    echo "${project_content/${project_whole_match}/$newValue}" >$project_file
     ;;
 *)
     echo "Unknown argument $1"
