019ab7c1338156121fc724bca8b25f0ba38d3b0a
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-05-17 12:42:01 +0200

    Update

---
+++ integration-test/shared/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    kotlin("multiplatform")
+    kotlin("native.cocoapods")
+    id("com.android.library")
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    cocoapods {
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        version = "1.0"
+        ios.deploymentTarget = "16.0"
+        podfile = project.file("../iosApp/Podfile")
+
+        pod("Sentry") {
+            version = Config.Libs.sentryCocoaVersion
+            extraOpts += listOf("-compiler-option", "-fmodules")
+        }
+
+        framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+    
+    sourceSets {
+        commonMain.dependencies {
+            val sentryVersion = properties["versionName"].toString()
+            implementation("io.sentry:sentry-kotlin-multiplatform:$sentryVersion")
+        }
+    }
+}
+
+android {
+    namespace = "sample.kmp.app"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
---
+++ integration-test/shared/src/androidMain/kotlin/sample/kmp/app/Platform.android.kt
@@ -0,0 +1,7 @@
+package sample.kmp.app
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
