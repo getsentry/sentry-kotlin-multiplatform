99104d4a262e594e089eb7c267a5fdd04303eb7f
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-06-05 21:02:33 +0200

    Updatee

--- plugin-build/plugin/src/main/java/io/sentry/kotlin/multiplatform/gradle/plugin/SentryExtension.kt
+++ plugin-build/plugin/src/main/java/io/sentry/kotlin/multiplatform/gradle/plugin/SentryExtension.kt
@@ -13,7 +13,13 @@ constructor(project: Project) {
     /**
      * Auto-installs the Sentry-Cocoa SDK pod if the cocoapods plugin is enabled.
      */
-    val autoInstallWithCocoapods: Property<Boolean> = objects.property(Boolean::class.java).convention(true)
+    val autoInstallWithCocoapods: Property<Boolean> =
+        objects.property(Boolean::class.java).convention(true)
 
+    /**
+     * Linker configuration.
+     *
+     * If you use SPM this configuration is necessary for setting up linking the framework and test executable.
+     */
     val linker: LinkerExtension = objects.newInstance(LinkerExtension::class.java, project)
 }
