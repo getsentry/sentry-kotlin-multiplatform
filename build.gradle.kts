ff7660c50ea0ecaec32ae3bcbe5a79020304b0c6
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2024-10-09 17:35:16 +0200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import com.diffplug.spotless.LineEnding
 import com.vanniktech.maven.publish.MavenPublishPlugin
 import com.vanniktech.maven.publish.MavenPublishPluginExtension
 import io.gitlab.arturbosch.detekt.Detekt
+import java.util.zip.ZipFile
 
 plugins {
     id(Config.gradleMavenPublishPlugin).version(Config.gradleMavenPublishPluginVersion)
@@ -15,7 +16,8 @@ plugins {
     id(Config.BuildPlugins.buildConfig).version(Config.BuildPlugins.buildConfigVersion).apply(false)
     kotlin(Config.kotlinSerializationPlugin).version(Config.kotlinVersion).apply(false)
     id(Config.QualityPlugins.kover).version(Config.QualityPlugins.koverVersion).apply(false)
-    id(Config.QualityPlugins.binaryCompatibility).version(Config.QualityPlugins.binaryCompatibilityVersion).apply(false)
+    id(Config.QualityPlugins.binaryCompatibility).version(Config.QualityPlugins.binaryCompatibilityVersion)
+        .apply(false)
 }
 
 allprojects {
@@ -56,6 +58,101 @@ subprojects {
                 // signing is done when uploading files to MC
                 // via gpg:sign-and-deploy-file (release.kts)
                 releaseSigningEnabled = false
+            }
+        }
+    }
+}
+
+tasks.register("validateDistributionArtifacts") {
+    subprojects {
+        val subproject = this@subprojects
+        if (subproject.name == "sentry-kotlin-multiplatform") {
+            validateKotlinMultiplatformCoreArtifacts(subproject)
+        }
+    }
+}
+
+private fun validateKotlinMultiplatformCoreArtifacts(project: Project) {
+    val rootDistributionFilePath =
+        "${project.layout.buildDirectory.asFile.get().path}${sep}distributions"
+    val expectedNumOfFiles = 15
+    val filesList = File(rootDistributionFilePath).listFiles()
+    val actualNumberOfFiles = filesList?.size ?: 0
+
+    if (actualNumberOfFiles == expectedNumOfFiles) {
+        println("✅ Found $actualNumberOfFiles distribution files as expected.")
+    } else {
+        throw IllegalStateException("❌ Expected $expectedNumOfFiles distribution files, but found $actualNumberOfFiles")
+    }
+
+    val baseFileName = "sentry-kotlin-multiplatform"
+    val platforms = listOf(
+        "watchosx64", "watchossimulatorarm64", "watchosarm64", "watchosarm32",
+        "tvosx64", "tvossimulatorarm64", "tvosarm64",
+        "macosx64", "macosarm64",
+        "jvm",
+        "iosx64", "iossimulatorarm64", "iosarm64",
+        "android"
+    )
+    val listOfArtifactPaths = buildList {
+        add("$rootDistributionFilePath$sep$baseFileName-$version.zip")
+        addAll(platforms.map { platform ->
+            "$rootDistributionFilePath$sep$baseFileName-$platform-$version.zip"
+        })
+    }
+
+    listOfArtifactPaths.forEach { artifactPath ->
+        val artifactFile = File(artifactPath)
+        if (!artifactFile.exists()) {
+            throw IllegalStateException("❌ Artifact file: $artifactPath does not exist")
+        }
+        if (artifactFile.length() == 0L) {
+            throw IllegalStateException("❌ Artifact file: $artifactPath is empty")
+        }
+
+        val file = File(artifactPath)
+        ZipFile(file).use { zip ->
+            val entries = zip.entries().asSequence().map { it.name }.toList()
+            val javadocFile = entries.firstOrNull { it.contains("javadoc") }
+            if (javadocFile == null) {
+                throw IllegalStateException("❌ javadoc file not found in ${file.name}")
+            }
+            val sourcesFile = entries.firstOrNull { it.contains("sources") }
+            if (sourcesFile == null) {
+                throw IllegalStateException("❌ sources file not found in ${file.name}")
+            }
+            val moduleFile = entries.firstOrNull { it.contains("module") }
+            if (moduleFile == null) {
+                throw IllegalStateException("❌ module file not found in ${file.name}")
+            }
+            val pomFile = entries.firstOrNull { it.contains("pom-default.xml") }
+            if (pomFile == null) {
+                throw IllegalStateException("❌ pom file not found in ${file.name}")
+            }
+
+            val isAppleArtifact =
+                file.name.contains("ios") || file.name.contains("macos") || file.name.contains("watchos") || file.name.contains(
+                    "tvos"
+                )
+            if (isAppleArtifact) {
+                // this is hardcoded but will probably not change unless we add another cinterop library or remove one
+                val expectedNumOfKlibFiles = 3
+                val actualKlibFiles = entries.filter { it.contains("klib") }
+                if (actualKlibFiles.size != expectedNumOfKlibFiles) {
+                    throw IllegalStateException("❌ $expectedNumOfKlibFiles klib files not found in ${file.name}")
+                } else {
+                    println("✅ Found $expectedNumOfKlibFiles klib files in ${file.name}")
+                }
+            }
+
+            val isAndroidArtifact = file.name.contains("android")
+            if (isAndroidArtifact) {
+                val aarFile = entries.firstOrNull() { it.contains("aar") }
+                if (aarFile == null) {
+                    throw IllegalStateException("❌ aar file not found in ${file.name}")
+                } else {
+                    println("✅ Found aar file in ${file.name}")
+                }
             }
         }
     }
