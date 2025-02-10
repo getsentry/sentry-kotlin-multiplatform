759f6fac31555971dcd2af28120757751588c2b9
GIancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2025-02-10 17:31:22 +0100

    use err output

--- sentry-kotlin-multiplatform-gradle-plugin/src/main/java/io/sentry/kotlin/multiplatform/gradle/DerivedDataPathValueSource.kt
+++ sentry-kotlin-multiplatform-gradle-plugin/src/main/java/io/sentry/kotlin/multiplatform/gradle/DerivedDataPathValueSource.kt
@@ -1,6 +1,10 @@
 package io.sentry.kotlin.multiplatform.gradle
 
+<<<<<<< Updated upstream
 import org.gradle.api.GradleException
+=======
+import io.sentry.kotlin.multiplatform.gradle.SentryPlugin.Companion.logger
+>>>>>>> Stashed changes
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.ValueSource
 import org.gradle.api.provider.ValueSourceParameters
@@ -25,7 +29,9 @@ internal abstract class DerivedDataPathValueSource :
 
     override fun obtain(): String {
         val buildDirOutput = ByteArrayOutputStream()
-        execOperations.exec {
+        val errOutput = ByteArrayOutputStream()
+
+        val execOperations = execOperations.exec {
             it.commandLine = listOf(
                 "xcodebuild",
                 "-project",
@@ -33,11 +39,33 @@ internal abstract class DerivedDataPathValueSource :
                 "-showBuildSettings"
             )
             it.standardOutput = buildDirOutput
+            it.errorOutput = errOutput
         }
+<<<<<<< Updated upstream
         val buildSettings = buildDirOutput.toString("UTF-8")
         val buildDirMatch = buildDirRegex.find(buildSettings)
         val buildDir = buildDirMatch?.groupValues?.get(1)
             ?: throw GradleException("BUILD_DIR not found in xcodebuild output")
         return buildDir.replace("/Build/Products", "")
+=======
+        if (execOperations.exitValue == 0) {
+            val buildSettings = buildDirOutput.toString("UTF-8")
+            val buildDirMatch = buildDirRegex.find(buildSettings)
+            val buildDir = buildDirMatch?.groupValues?.get(1)
+            if (buildDir == null || buildDir.contains("DerivedData").not()) {
+                return null
+            }
+            return buildDir.replace("/Build/Products", "")
+        } else {
+            logger.warn(
+                "Failed to retrieve derived data path. xcodebuild command failed. Error output: ${
+                    errOutput.toString(
+                        Charsets.UTF_8
+                    )
+                }"
+            )
+            return null
+        }
+>>>>>>> Stashed changes
     }
 }
