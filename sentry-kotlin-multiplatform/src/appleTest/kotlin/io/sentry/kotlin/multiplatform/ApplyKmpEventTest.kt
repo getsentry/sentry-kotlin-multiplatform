35275a69058acdac62c88088d9c2cf0d11ee067e
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-06-26 02:29:17 +0200

    Fix apple test

--- sentry-kotlin-multiplatform/src/appleTest/kotlin/io/sentry/kotlin/multiplatform/ApplyKmpEventTest.kt
+++ sentry-kotlin-multiplatform/src/appleTest/kotlin/io/sentry/kotlin/multiplatform/ApplyKmpEventTest.kt
@@ -2,7 +2,9 @@ package io.sentry.kotlin.multiplatform
 
 import io.sentry.kotlin.multiplatform.extensions.applyKmpEvent
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class ApplyKmpEventTest {
     @Test
@@ -16,8 +18,9 @@ class ApplyKmpEventTest {
 
         nativeEvent.applyKmpEvent(kmpEvent)
 
-        assertFalse(nativeEvent.wasReleaseSet)
+        assertEquals(1, nativeEvent.releaseSetCount)
     }
+
 
     @Test
     fun `native dist is not set if kmp dist has same value`() {
@@ -30,23 +33,51 @@ class ApplyKmpEventTest {
 
         nativeEvent.applyKmpEvent(kmpEvent)
 
-        assertFalse(nativeEvent.wasDistSet)
+        assertEquals(1, nativeEvent.distSetCount)
+    }
+
+    @Test
+    fun `native release is set if kmp release has different value`() {
+        val nativeEvent = FakeSentryEvent().apply {
+            releaseName = "1.0.0"
+        }
+        val kmpEvent = SentryEvent().apply {
+            release = "7.0.0"
+        }
+
+        nativeEvent.applyKmpEvent(kmpEvent)
+
+        assertEquals(2, nativeEvent.releaseSetCount)
+    }
+
+    @Test
+    fun `native dist is set if kmp dist has different value`() {
+        val nativeEvent = FakeSentryEvent().apply {
+            dist = "randomDist"
+        }
+        val kmpEvent = SentryEvent().apply {
+            dist = "differentDist"
+        }
+
+        nativeEvent.applyKmpEvent(kmpEvent)
+
+        assertEquals(2, nativeEvent.distSetCount)
     }
 }
 
 // init with a SentryLevel as a workaround for:
 // Unable to call non-designated initializer as super constructor
 private class FakeSentryEvent : CocoaSentryEvent(Internal.Sentry.kSentryLevelFatal) {
-    var wasReleaseSet = false
-    var wasDistSet = false
+    var releaseSetCount = 0
+    var distSetCount = 0
 
     override fun setReleaseName(releaseName: String?) {
         super.setReleaseName(releaseName)
-        wasReleaseSet = true
+        releaseSetCount += 1
     }
 
     override fun setDist(dist: String?) {
         super.setDist(dist)
-        wasDistSet = true
+        distSetCount += 1
     }
 }
