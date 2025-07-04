31e24c638796a7901343629ffda624e8f98dbc76
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-07-04 10:37:42 +0200

    Add toolchain to java block

--- sentry-samples/kmp-app-cocoapods/androidApp/build.gradle.kts
+++ sentry-samples/kmp-app-cocoapods/androidApp/build.gradle.kts
@@ -40,6 +40,10 @@ android {
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
 
 kotlin {
--- sentry-samples/kmp-app-spm/androidApp/build.gradle.kts
+++ sentry-samples/kmp-app-spm/androidApp/build.gradle.kts
@@ -9,6 +9,10 @@ plugins {
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
 
 kotlin {
