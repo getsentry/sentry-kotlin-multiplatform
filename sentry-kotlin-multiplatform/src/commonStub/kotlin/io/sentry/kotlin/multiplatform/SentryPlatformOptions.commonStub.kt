aa7e7f6dc39054fb6e8c9c05e40ae4b95a6b8fac
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2025-07-21 14:36:24 +0200

    Update SentryPlatformOptions.commonStub.kt

--- sentry-kotlin-multiplatform/src/commonStub/kotlin/io/sentry/kotlin/multiplatform/SentryPlatformOptions.commonStub.kt
+++ sentry-kotlin-multiplatform/src/commonStub/kotlin/io/sentry/kotlin/multiplatform/SentryPlatformOptions.commonStub.kt
@@ -3,8 +3,10 @@ package io.sentry.kotlin.multiplatform
 public actual class SentryPlatformOptions
 
 internal actual fun SentryPlatformOptions.prepareForInit() {
-    // no-op on JS
+    // No-op
 }
 
 internal actual fun SentryOptions.toPlatformOptionsConfiguration(): PlatformOptionsConfiguration =
-    { /* no-op for JS target */ }
+    {
+            // No-op
+    }
